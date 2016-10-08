f85a76867c75afc98958f6ec272e7651c19a4692
Joshua Feiber <jfeiber@fangio.kiewit.dartmouth.edu>
2016-10-08 19:57:09 -0400

    Code to make things buzz

--- buzzer/BuzzerFSMCallbacks.h
+++ buzzer/BuzzerFSMCallbacks.h
@@ -1,15 +1,25 @@
 #ifndef BUZZERFSMCALLBACKS_H
 #define BUZZERFSMCALLBACKS_H
 
-int SetupFunc(int elapsed_time_in_curr_state) {
-  Serial.println("this is a test1");
+#include "Pins.h"
+#include <Arduino.h>
+
+int InitFunc(unsigned long state_start_time, int num_iterations_in_state) {
+  Serial.println("I will wait for 2 seconds");
   delay(2000);
   return SUCCESS;
 }
 
-int OtherFunc(int elapsed_time_in_curr_state) {
-  Serial.println("this is another test!");
-  delay(2000);
+int BuzzerOnFunc(unsigned long state_start_time, int num_iterations_in_state) {
+  analogWrite(BUZZER_PIN, 255);
+  Serial.println(millis());
+  Serial.println(state_start_time);
+  if ( (millis() - state_start_time)/1000 >= 2 ) return SUCCESS;
+  return FAILURE;
+}
+
+int BuzzerOffFunc(unsigned long state_start_time, int num_iterations_in_state) {
+  analogWrite(BUZZER_PIN, 0);
   return SUCCESS;
 }
 
--- buzzer/buzzer.ino
+++ buzzer/buzzer.ino
@@ -1,11 +1,14 @@
 #include "BuzzerFSM.h"
 #include "BuzzerFSMCallbacks.h"
+#include "Pins.h"
 
-BuzzerFSM buzzer_fsm({OTHERSTATE, SETUP, SetupFunc}, SETUP);
+BuzzerFSM buzzer_fsm({BUZZER_ON, BUZZER_ON, InitFunc}, INIT);
 
 void setup() {
   Serial.begin(9600);
-  buzzer_fsm.AddState({SETUP, SETUP, OtherFunc}, OTHERSTATE);
+  pinMode(BUZZER_PIN, OUTPUT);
+  buzzer_fsm.AddState({BUZZER_OFF, BUZZER_ON, BuzzerOnFunc}, BUZZER_ON);
+  buzzer_fsm.AddState({INIT, INIT, BuzzerOffFunc}, BUZZER_OFF);
 }
 
 void loop() {
