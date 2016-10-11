083e1f068d991f9227f06f7d1e607b442f3f942d
Joshua Feiber <jfeiber@fangio.kiewit.dartmouth.edu>
2016-10-11 00:12:56 -0400

    Beginnings of class to wrap cell modem functions

--- buzzer/Pins.h
+++ buzzer/Pins.h
@@ -1,6 +1,9 @@
 #ifndef PINS_H
 #define PINS_H
 
-#define BUZZER_PIN 3
+#define BUZZER_PIN 6
+#define FONA_RX_PIN 2
+#define FONA_TX_PIN 3
+#define FONA_RST_PIN 4
 
 #endif
--- buzzer/buzzer.ino
+++ buzzer/buzzer.ino
@@ -1,17 +1,24 @@
+#include <SoftwareSerial.h>
 #include "BuzzerFSM.h"
 #include "BuzzerFSMCallbacks.h"
+#include "FonaShield.h"
 #include "Pins.h"
 
 BuzzerFSM buzzer_fsm({BUZZER_ON, BUZZER_ON, InitFunc}, INIT);
+SoftwareSerial fona_serial = SoftwareSerial(FONA_TX_PIN, FONA_RX_PIN);
+FonaShield fona_shield(&fona_serial, FONA_RST_PIN);
 
 void setup() {
-  Serial.begin(9600);
-  pinMode(BUZZER_PIN, OUTPUT);
-  buzzer_fsm.AddState({BUZZER_OFF, BUZZER_ON, BuzzerOnFunc}, BUZZER_ON);
-  buzzer_fsm.AddState({INIT, INIT, BuzzerOffFunc}, BUZZER_OFF);
+  Serial.begin(115200);
+  pinMode(FONA_RST_PIN, OUTPUT);
+  // pinMode(BUZZER_PIN, OUTPUT);
+  // buzzer_fsm.AddState({BUZZER_OFF, BUZZER_ON, BuzzerOnFunc}, BUZZER_ON);
+  // buzzer_fsm.AddState({INIT, INIT, BuzzerOffFunc}, BUZZER_OFF);
 }
 
 void loop() {
-  Serial.println("hallo");
-  buzzer_fsm.ProcessState();
+  // Serial.println("hallo");
+  // buzzer_fsm.ProcessState();
+  fona_shield.Init();
+  Serial.println("Done setting up");
 }
