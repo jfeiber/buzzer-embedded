835d47576214bc050947b8b92a2b32bbd4c8c76f
Josh Feiber <joshua@feiber.com>
2016-10-15 15:47:27 -0400

    FonaShield: Added ability to perform an HTTP GET. The method will return one line of the response which is perfect for API endpoints that return minified JSON.

--- buzzer/FonaShield.h
+++ buzzer/FonaShield.h
@@ -9,7 +9,8 @@ typedef const __FlashStringHelper * FlashStrPtr;
 #define _max_line_length 256
 #define _max_init_retires 5
 #define APN "wholesale"
-#define OK_REPLY F("OK")
+#define NEW_LINE_BYTES "\r\xA"
+#define OK_REPLY F(NEW_LINE_BYTES "OK" NEW_LINE_BYTES)
 
 #define DEBUG_PRINT_FLASH(str) Serial.print(F(str))
 #define DEBUG_PRINTLN_FLASH(str) Serial.println(F(str))
@@ -24,14 +25,23 @@ class FonaShield {
     int _rst_pin;
     bool readAvailBytesFromSerial(char *buffer, int buffer_len, unsigned long timeout);
     void resetShield();
-    void sendATCommand(FlashStrPtr command);
+    void sendATCommand(FlashStrPtr command, bool use_newline = true);
     bool sendATCommandCheckReply(FlashStrPtr command, FlashStrPtr expected_reply, unsigned long timeout = 50);
+    bool sendATCommandParamCheckReply(FlashStrPtr command, FlashStrPtr param, FlashStrPtr expected_reply, unsigned long timeout = 50);
+    bool sendATCommandCheckReply(FlashStrPtr command, char *buffer, int buffer_len, FlashStrPtr expected_reply, unsigned long timeout = 50);
+    bool sendATCommandCheckAck(FlashStrPtr command, unsigned long timeout = 50);
     bool checkATCommandReply(FlashStrPtr expected_reply, unsigned long timeout);
-    bool sendATCommandCheckAck(FlashStrPtr command, unsigned long timeout);
+    bool checkATCommandReply(FlashStrPtr expected_reply, char *rep_buffer, int buffer_len, unsigned long timeout);
+    int getHTTPStatusFromRes(char *rep_buffer);
+    int isEqual(char *buf1, FlashStrPtr buf2);
+    bool getOneLineHTTPReplyFromRes(char *at_res_buffer, int at_res_buffer_len, char *http_res_buffer, int http_res_buffer_len);
+    bool HTTPInit(FlashStrPtr URL);
+    bool HTTPFail();
   public:
     FonaShield(SoftwareSerial *fona_serial, int rst_pin);
     bool initShield();
     bool enableGPRS();
+    bool HTTPGETOneLine(FlashStrPtr URL, char *http_res_buffer, int http_res_buffer_len);
 };
 
 #endif
